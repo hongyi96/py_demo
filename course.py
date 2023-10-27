09d74a7ed0892cd122adcbd6f1e0e8d27aeb1ba2
hongyi <940810647@qq.com>
2023-10-27 11:43:13 +0800

    初中课程获取

--- course.py
+++ course.py
@@ -4,7 +4,7 @@ import requests
 import json
 import time
 
-Cookie = 'company_server_type=workwechat; company_code=jste; company_old_code=3c851aa4a4b211ea9b18525400edef21; company_display_name=%E6%B1%9F%E8%8B%8F%E6%95%99%E5%B8%88; ti18nLng=zh-CN; token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvbGV4aWFuZ2xhLmNvbVwvc3VpdGVzXC9hdXRoLWNhbGxiYWNrIiwiaWF0IjoxNjk3OTYyOTgxLCJleHAiOjE3MDA1NTQ5ODEsIm5iZiI6MTY5Nzk2Mjk4MSwianRpIjoiaDR4cER4UjdiVVJpTW81MyIsInN1YiI6IjY4ZmZhYTM4MTUzNDExZWNhZTc5MTY4NjRiYzYzYTg2IiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyIsImNvbXBhbnlfaWQiOiIzYzg1MjUwOGE0YjIxMWVhODQ3MzUyNTQwMGVkZWYyMSIsInN0YWZmX3V1aWQiOiI2OGVhZmE3YTE1MzQxMWVjYjg2NzE2ODY0YmM2M2E4NiJ9.23p6y1voYw8cdLKMeewNDPWqG8KyVogn1gHlesogDGk; XSRF-TOKEN=vVRBsMP6sDrjp69aGGRkt%252Bz7biBSaHnNmVdb8tNoCepHkn4XexVAMTEjYP3%252FrCqcDkMnirkFZ37sScFnhdoddXZ9%252FQ01fvJOPShXtLPwcVM%253D'
+Cookie = 'token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvbGV4aWFuZ2xhLmNvbVwvc3VpdGVzXC9hdXRoLWNhbGxiYWNrIiwiaWF0IjoxNjk4Mzc3MDUxLCJleHAiOjE3MDA5NjkwNTEsIm5iZiI6MTY5ODM3NzA1MSwianRpIjoiYWFoTUZmdkhha1N0UGNocSIsInN1YiI6ImI1ZmYyM2Y4YTY0NjExZWE4OGQxNTI1NDAwZWRlZjIxIiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyIsImNvbXBhbnlfaWQiOiIzYzg1MjUwOGE0YjIxMWVhODQ3MzUyNTQwMGVkZWYyMSIsInN0YWZmX3V1aWQiOiJiNWY0YzM3MmE2NDYxMWVhODE0NTUyNTQwMGVkZWYyMSJ9.lgNoUVDTfnV0Fa838z59myluQ2e_3MPy_Okhy7krtwQ; company_server_type=workwechat; company_code=jste; company_old_code=3c851aa4a4b211ea9b18525400edef21; company_display_name=%E6%B1%9F%E8%8B%8F%E6%95%99%E5%B8%88; ti18nLng=zh-CN; XSRF-TOKEN=nlbkwLlwQSzqQxG70ZuJmK7%252BWbqfywm7PrjDK%252BUf4Hps4ZfOiUW0FLKfFkt0gheFTMW7IhpwUv%252B6TYBmjyoHVPf9KiqDJWQQUDHUzlvVipE%253D'
 
 
 def list_chapter():
@@ -26,6 +26,31 @@ def list_chapter():
 
     print(ids)
     return ids[:8]
+
+
+def list_middle_chapter():
+    url = "https://jste.lexiangla.com/api/v1/classes/b5e8284467d911eeb0af66f2c73c8c41?with_source_detail=1"
+    headers = {
+        'Cookie': Cookie,
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("GET", url, headers=headers, verify=False)
+
+    json_data = json.loads(response.text)
+    chapters = json_data['data']['chapters']
+    ids = []
+    titles = []
+    for chapter in chapters:
+        if chapter['id'] == 359:
+            courses = chapter['courses']
+            for course in courses:
+                titles.append(course['title'])
+                ids.append(course['id'])
+
+    print(titles)
+    print(ids)
+    return ids
 
 
 def get_time():
@@ -89,7 +114,10 @@ def auto():
     if learned_time > 400 * 60:
         print('学习完成')
         return
-    course_ids = list_chapter()
+
+    # 选择课程
+    course_ids = list_middle_chapter()
+    # course_ids = list_chapter()
 
     for courses_id in course_ids:
         # 终止一次全部课程防止重复学习
@@ -105,4 +133,5 @@ def auto():
 
 if __name__ == '__main__':
     # start('52450a12682111eeae869ab385c8785b')
+    # list_middle_chapter()
     auto()
